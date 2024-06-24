99902ef7681b82a16f9fb71f1e222853fc9cd72c
NinoPapava <gv.papava@gmail.com>
2024-06-24 12:26:23 +0400

    Add Home component

--- src/components/HomeComponent/HomePage.tsx
+++ src/components/HomeComponent/HomePage.tsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { Carousel } from 'antd';
+import '../../../styles/home.css'
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div>
+      <h2>Home Page</h2>
+      <Carousel autoplay autoplaySpeed={3000} dots={true}>
+        <div>
+          <h3>Slide 1</h3>
+          <img src="/images/team2.jpg" alt="team img" className="carousel-image" />
+        </div>
+        <div>
+          <h3>Slide 2</h3>
+          <img src="/images/team4.jpeg" alt="team img" className="carousel-image" />
+        </div>
+        <div>
+          <h3>Slide 3</h3>
+          <img src="/images/team3.jpg" alt="team img" className="carousel-image" />
+        </div>
+      </Carousel>
+    </div>
   )
 }
 
